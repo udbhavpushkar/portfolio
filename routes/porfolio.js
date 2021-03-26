b926347ba95dee35129844450cbc8a324dbdf0a3
udbhavpushkar <udbhavpushkar@gmail.com>
2021-03-26 17:06:55 +0530

    new c

--- routes/porfolio.js
+++ routes/porfolio.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
         const portfolio = await Security.find()
         const currentPrice = 100 //Assuming current price = 100.00 (given in the task)
         const returns = portfolio.reduce((ans, value)=>ans+((currentPrice-value.price)*value.quantity), 0)
-        res.send({returns: returns})
+        res.json({returns: returns})
     }catch (err){
         res.json({message: err.message})
     }
